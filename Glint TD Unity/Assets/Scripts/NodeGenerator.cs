fddc31c88da9eb2a3f0e3d52130865adcb2967e3
jalfrey <56270827+jalfrey@users.noreply.github.com>
2022-04-04 16:42:01 -0500

    Path Generation Init

---
+++ Glint TD Unity/Assets/Scripts/NodeGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeGenerator : MonoBehaviour
+{
+    [SerializeField] public List<Transform> pointsList = new List<Transform>();
+    private float interpolate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
