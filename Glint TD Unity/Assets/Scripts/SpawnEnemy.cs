10fa3967679da127d221b44b654d872e8de41cbf
jalfrey <56270827+jalfrey@users.noreply.github.com>
2022-04-18 16:39:44 -0500

    Enemy Path

--- Glint TD Unity/Assets/Scripts/SpawnEnemy.cs
+++ Glint TD Unity/Assets/Scripts/SpawnEnemy.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
+    public float enemySpeed = 5f;
+    public float enemyHealth = 10f;
+    private Transform dest;
+    private int initialIndex; 
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dest = WaypointBehavior.pointsArray[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 direction = dest.position - transform.position; 
+        transform.Translate 
     }
 }
