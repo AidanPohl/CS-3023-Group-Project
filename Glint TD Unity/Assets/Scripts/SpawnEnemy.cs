de783bf24833939fdac6c9f764e0df1b5317d761
stapleton <56270816+garrondenney74@users.noreply.github.com>
2022-04-24 23:22:47 -0500

    spawnEnemy

--- Glint TD Unity/Assets/Scripts/SpawnEnemy.cs
+++ Glint TD Unity/Assets/Scripts/SpawnEnemy.cs
@@ -1,23 +1,58 @@
+//used for implementing a survival based enemy deployment system
+
+
+
+
+
+
+
+
+
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 public class SpawnEnemy : MonoBehaviour
 {
     public float enemySpeed = 5f;
     public float enemyHealth = 10f;
-    private Transform dest;
+    public float spawnInteval = 2f;
+    public List<GameObject> prefabs;
+
+
+    public static Stopwatch timer = new Stopwatch();
+    public GameObject enemy;
+    public Transform start;
     private int initialIndex; 
     
 
+
+
+    public void StartSpawning()
+    {
+        //call spawn handler
+        StartCoroutine(SpawnDelay());
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        dest = WaypointBehavior.pointsArray[0];
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnDelay()
     {
-        Vector3 direction = dest.position - transform.position; 
-        //transform.Translate 
+        sEnemy();
+        yield return new WaitForSeconds(spawnInteval);
+        StartCoroutine(SpawnDelay());
+
     }
+
+
+    void sEnemy()
+    {
+        GameObject spawnedEnemy = Instantiate(enemy, start);
+    }
+
 }
